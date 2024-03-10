a0c7780d32cac5d117adb01aa9dfbaeaa67aee7d
daewon_x_kim <daewon_x_kim@bip.co.jp>
2024-03-11 03:21:58 +0900

    dast110321

--- src/testapp/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-
-class Post(models.Model):
-    name = models.CharField('user name', max_length=15)
-    micropost = models.CharField('tweet', max_length=140, blank=True)
-
-    def __str__(self):
-        return self.name
